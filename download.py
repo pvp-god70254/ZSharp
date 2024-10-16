dea4b8a1d5d691783050db6919495fded6e5b720
pvp-god70254 <PVPGOD70254@gmail.com>
2024-10-16 16:19:05 -0500

    Update download.py

--- download.py
+++ download.py
@@ -6,5 +6,6 @@ os.system('curl -O https://raw.githubusercontent.com/pvp-god70254/ZSharp/refs/he
 open('compiler.js', 'w').write("import { genZ } from './ZS.js'\n\n  var repeat;\n\n  async function readFile(path) {\n    const file = await fetch(path)\n\n    const text = await file.text()\n\n    return text\n  }\n\n  const code = await readFile('../script.zs')\n\n  new genZ(code).$(repeat)")
 
 
-
+#DOWNLOAD SIGNAL
 print(f'\033[1;32mDOWNLOADED\033[0;0m')
+
